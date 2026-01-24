c1894458001cd233f8edce0f7ff112876f7ebdaf
Nihad Abdulla <nihadabdulla@Nihads-MacBook-Air.local>
2026-01-24 15:05:54 +0530

    testimonial commanded

--- app/page.tsx
+++ app/page.tsx
@@ -219,7 +219,7 @@ export default function Home() {
       <PartnerLogos />
       <FAQSection />
 
-      <TestimonialsSection />
+      {/* <TestimonialsSection /> */}
       <MagneticLogoGrid />
     </>
   );
